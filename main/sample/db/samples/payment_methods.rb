34186a80b4857b856597921d4355b27249fc46a3
AmirHossein Aghdassi <amirh.aghdassi@gmail.com>
2015-12-10 12:21:48 +0330

    Rewrite sample data
    - Payment methods
    - Shipping categories and shipping methods
    - Tax categories and tax rates

--- main/sample/db/samples/payment_methods.rb
+++ main/sample/db/samples/payment_methods.rb
@@ -1,15 +1,31 @@
+Spree::PaymentMethod::Check.create!(
+  {
+    name: "نقدی",
+    description: "پرداخت با استفاده از پول نقد",
+    active: true
+  }
+)
+
 Spree::Gateway::Bogus.create!(
   {
-    name: "Credit Card",
-    description: "Bogus payment gateway.",
+    name: "کارت بانکی",
+    description: "پرداخت با استفاده از دستگاه کارت‌خوان و کارت‌های بانکی عضو شتاب",
     active: true
   }
 )
 
 Spree::PaymentMethod::Check.create!(
   {
-    name: "Check",
-    description: "Pay by check.",
+    name: "چک",
+    description: "پرداخت با استفاده از چک",
+    active: true
+  }
+)
+
+Spree::Gateway::Bogus.create!(
+  {
+    name: "اینترنتی",
+    description: "پرداخت با استفاده از درگاه پرداخت اینترنتی و کارت‌های بانکی عضو شتاب",
     active: true
   }
 )
--- main/sample/db/samples/tax_rates.rb
+++ main/sample/db/samples/tax_rates.rb
@@ -1,9 +1,9 @@
-north_america = Spree::Zone.find_by_name!("North America")
-clothing = Spree::TaxCategory.find_by_name!("Clothing")
+default_zone = Spree::Zone.find_by_name!("پیش‌فرض")
+vat_tax = Spree::TaxCategory.find_by_name!("مالیات بر ارزش افزوده (VAT)")
 tax_rate = Spree::TaxRate.create(
-  :name => "North America",
-  :zone => north_america, 
-  :amount => 0.05,
-  :tax_category => clothing)
+  :name => "مالیات پیش‌فرض",
+  :zone => default_zone, 
+  :amount => 0.06,
+  :tax_category => vat_tax)
 tax_rate.calculator = Spree::Calculator::DefaultTax.create!
 tax_rate.save!
